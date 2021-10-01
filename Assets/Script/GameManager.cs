94cfd1d571a0656336f64706dd21d24dc00109ab
rezelion <ahmadspade2014@gmail.com>
2021-10-01 22:48:25 +0700

    Script Cloud Save

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+    [Range(0f, 1f)]
+    public float AutoCollectPercentage = 0.1f;
+    public ResourceConfig[] ResourcesConfigs;
+    public Sprite[] ResourcesSprites;
+    public Transform ResourcesParent;
+    public ResourceController ResourcePrefab;
+    public TapText TapTextPrefab;
+    public Transform CoinIcon;
+    public Text GoldInfo;
+    public Text AutoCollectInfo;
+    public float SaveDelay = 5f;
+    private List<ResourceController> _activeResources = new List<ResourceController>();
+    private List<TapText> _tapTextPool = new List<TapText>();
+    private float _collectSecond;
+    private float _saveDelayCounter;
+    public double _totalGold; //private
+    public AudioSource coinFX;
+
+    private void Start()
+    {
+        AddAllResources();
+
+        //GoldInfo to String
+        GoldInfo.text = $"Gold:{UserDataManager.Progress.Gold.ToString("0")}";
+    }
+    private void Update()
+    {
+        float deltaTime = Time.unscaledDeltaTime;
+        _saveDelayCounter -= deltaTime;
+
+        // Fungsi untuk selalu mengeksekusi CollectPerSecond setiap detik
+        _collectSecond += deltaTime;
+        //_collectSecond += Time.unscaledDeltaTime;
+        if (_collectSecond >= 1f)
+        {
+            CollectPerSecond();
+            _collectSecond = 0f;
+        }
+        CheckResourceCost();
+
+        CoinIcon.transform.localScale = Vector3.LerpUnclamped(CoinIcon.transform.localScale, Vector3.one * 2f, 0.15f);
+        CoinIcon.transform.Rotate(0f, 0f, Time.deltaTime * -100f);
+
+    }
+    private void CollectPerSecond()
+    {
+        
+        double output = 0;
+        foreach (ResourceController resource in _activeResources)
+        {
+            if (resource.IsUnlocked)
+            {
+                output += resource.GetOutput();
+            }
+        }
+        output *= AutoCollectPercentage;
+
+        // Fungsi ToString("F1") ialah membulatkan angka menjadi desimal yang memiliki 1 angka di belakang koma 
+
+        AutoCollectInfo.text = $"Auto Collect: { output.ToString("F1") } / second";
+        AddGold(output);
+
+    }
+
+    //before private
+    public void AddGold(double value)
+    {
+        UserDataManager.Progress.Gold += value;
+        //_totalGold += value;
+        //GoldInfo.text = $"Gold: { _totalGold.ToString("0") }";
+        GoldInfo.text = $"Gold:{UserDataManager.Progress.Gold.ToString("0")}";
+        UserDataManager.Save(_saveDelayCounter < 0f);
+        //UserDataManager.Save();
+        if(_saveDelayCounter < 0f)
+        {
+            _saveDelayCounter = SaveDelay;
+        }
+    }
+
+    public void CollectByTap(Vector3 tapPosition, Transform parent)
+    {
+        coinFX.PlayOneShot(coinFX.clip);
+        double output = 0;
+
+        foreach (ResourceController resource in _activeResources)
+        {
+            if (resource.IsUnlocked)
+            {
+                output += resource.GetOutput();
+            }
+        }
+
+        TapText tapText = GetOrCreateTapText();
+        tapText.transform.SetParent(parent, false);
+        tapText.transform.position = tapPosition;
+        tapText.Text.text = $"+{ output.ToString("0") }";
+        tapText.gameObject.SetActive(true);
+        CoinIcon.transform.localScale = Vector3.one * 1.75f;
+        AddGold(output);
+    }
+    private TapText GetOrCreateTapText()
+    {
+        TapText tapText = _tapTextPool.Find(t => !t.gameObject.activeSelf);
+        if (tapText == null)
+        {
+            tapText = Instantiate(TapTextPrefab).GetComponent<TapText>();
+            _tapTextPool.Add(tapText);
+
+        }
+        return tapText;
+    }
+    private void AddAllResources()
+    {
+        bool showResources = true;
+        int index = 0;
+        foreach (ResourceConfig config in ResourcesConfigs)
+        {
+            GameObject obj = Instantiate(ResourcePrefab.gameObject, ResourcesParent, false);
+            ResourceController resource = obj.GetComponent<ResourceController>();
+            resource.SetConfig(index, config);
+            obj.gameObject.SetActive(showResources);
+            if (showResources && !resource.IsUnlocked)
+            {
+                showResources = false;
+            }
+            _activeResources.Add(resource);
+            index++;
+        }
+    }
+    public void ShowNextResource()
+    {
+        foreach (ResourceController resource in _activeResources)
+        {
+            if (!resource.gameObject.activeSelf)
+            {
+                resource.gameObject.SetActive(true);
+                break;
+            }
+        }
+    }
+    private void CheckResourceCost()
+    {
+        foreach (ResourceController resource in _activeResources)
+        {
+            bool isBuyable = false;
+            if (resource.IsUnlocked)
+            {
+                //isBuyable = _totalGold >= resource.GetUpgradeCost();
+                isBuyable = UserDataManager.Progress.Gold >= resource.GetUpgradeCost();
+            }
+            else
+            {
+                //isBuyable = _totalGold >= resource.GetUnlockCost();
+                isBuyable = UserDataManager.Progress.Gold >= resource.GetUnlockCost();
+            }
+            resource.ResourceImage.sprite = ResourcesSprites[isBuyable ? 1 : 0];
+        }
+    }
+}
+[System.Serializable]
+public struct ResourceConfig
+{
+    public string Name;
+    public double UnlockCost;
+    public double UpgradeCost;
+    public double Output;
+}
---
+++ Assets/Script/UserDataManager.cs
@@ -0,0 +1,111 @@
+﻿using Firebase.Storage;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class UserDataManager : MonoBehaviour
+{
+    private const string PROGRESS_KEY = "Progress";
+    public static UserProgressData Progress;
+
+    /*public static void Load()
+    {
+        // Cek apakah ada data yang tersimpan sebagai PROGRESS_KEY
+        if (!PlayerPrefs.HasKey(PROGRESS_KEY))
+        {
+            // Jika tidak ada, maka buat data baru
+            Progress = new UserProgressData();
+            Save();
+        }
+        else
+        {
+            // Jika ada, maka timpa progress dengan yang sebelumnya
+            string json = PlayerPrefs.GetString(PROGRESS_KEY);
+            Progress = JsonUtility.FromJson<UserProgressData>(json);
+        }
+    }
+
+    public static void Save()
+    {
+        string json = JsonUtility.ToJson(Progress);
+        PlayerPrefs.SetString(PROGRESS_KEY, json);
+    }*/
+
+    public static void LoadFromLocal()
+    {
+        // Cek apakah ada data yang tersimpan sebagai PROGRESS_KEY
+        if (!PlayerPrefs.HasKey(PROGRESS_KEY))
+        {
+            // Jika tidak ada, maka buat data baru
+            Progress = new UserProgressData();
+            // dan upload ke Cloud
+            Save(true);
+        }
+        else
+        {
+            // Jika ada, maka timpa progress dengan yang sebelumnya
+            string json = PlayerPrefs.GetString(PROGRESS_KEY);
+            Progress = JsonUtility.FromJson<UserProgressData>(json);
+        }
+    }
+
+    public static IEnumerator LoadFromCloud(System.Action onComplete)
+    {
+        StorageReference targetStorage = GetTargetCloudStorage();
+
+        bool isCompleted = false;
+        bool isSuccessfull = false;
+        const long maxAllowedSize = 1024 * 1024;// = 1 MB
+        targetStorage.GetBytesAsync(maxAllowedSize).ContinueWith((Task <byte[]> task)=> {
+            if (!task.IsFaulted)
+            {
+                string json = Encoding.Default.GetString(task.Result);
+                Progress = JsonUtility.FromJson<UserProgressData>(json);
+                isSuccessfull = true;
+            }
+            isCompleted = true;
+        });
+
+        while (!isCompleted)
+        {
+            yield return null;
+        }
+        // Jika sukses mengunduh, maka simpan data hasil unduh
+        if (isSuccessfull)
+        {
+            Save();
+        }
+        else
+        {
+            //Jika tidak ada data di cloud, maka load data dari local
+            LoadFromLocal();
+        }
+        onComplete?.Invoke();
+    }
+    public static void Save(bool uploadToCloud = false)
+    {
+        string json = JsonUtility.ToJson(Progress);
+        PlayerPrefs.SetString(PROGRESS_KEY, json);
+        if (uploadToCloud)
+        {
+            AnalyticsManager.SetUserProperties("gold", Progress.Gold.ToString());
+            byte[] data = Encoding.Default.GetBytes(json);
+            StorageReference targetStorage = GetTargetCloudStorage();
+
+            targetStorage.PutBytesAsync(data);
+        }
+    }
+    private static StorageReference GetTargetCloudStorage()
+    {
+        // Gunakan Device ID sebagai nama file yang akan disimpan di cloud
+        string deviceID = SystemInfo.deviceUniqueIdentifier;
+
+        FirebaseStorage storage = FirebaseStorage.DefaultInstance;
+        return storage.GetReferenceFromUrl($"{storage.RootReference}/{deviceID}");
+    }
+    public static bool HasResources(int index)
+    {
+        return index + 1 <= Progress.ResourcesLevels.Count;
+    }
+}
