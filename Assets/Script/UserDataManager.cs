c8d0af96525a38025998839213aa0250897583fa
rezelion <ahmadspade2014@gmail.com>
2021-10-02 01:40:10 +0700

    Fixing Error & Update Load/Save Cloud

--- Assets/Script/UserDataManager.cs
+++ Assets/Script/UserDataManager.cs
@@ -9,30 +9,7 @@ public class UserDataManager : MonoBehaviour
     private const string PROGRESS_KEY = "Progress";
     public static UserProgressData Progress;
 
-    public static void Load()
-    {
-        // Cek apakah ada data yang tersimpan sebagai PROGRESS_KEY
-        if (!PlayerPrefs.HasKey(PROGRESS_KEY))
-        {
-            // Jika tidak ada, maka buat data baru
-            Progress = new UserProgressData();
-            Save();
-        }
-        else
-        {
-            // Jika ada, maka timpa progress dengan yang sebelumnya
-            string json = PlayerPrefs.GetString(PROGRESS_KEY);
-            Progress = JsonUtility.FromJson<UserProgressData>(json);
-        }
-    }
-
-    public static void Save()
-    {
-        string json = JsonUtility.ToJson(Progress);
-        PlayerPrefs.SetString(PROGRESS_KEY, json);
-    }
-
-    /*public static void LoadFromLocal()
+    public static void LoadFromLocal()
     {
         // Cek apakah ada data yang tersimpan sebagai PROGRESS_KEY
         if (!PlayerPrefs.HasKey(PROGRESS_KEY))
@@ -103,10 +80,7 @@ public class UserDataManager : MonoBehaviour
 
         FirebaseStorage storage = FirebaseStorage.DefaultInstance;
         return storage.GetReferenceFromUrl($"{storage.RootReference}/{deviceID}");
-    }*/
-
-
-
+    }
     public static bool HasResources(int index)
     {
         return index + 1 <= Progress.ResourcesLevels.Count;
